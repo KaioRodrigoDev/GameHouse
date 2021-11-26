f9cb93769e3d18bbf7b946aa0799aa78fc0e7c9a
NerdInGame <eunumseidenad@gmail.com>
2021-11-26 14:51:03 -0300

    Botao do discord pronto

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SignIn } from './src/screens/SignIn';
 
+
 export default function app(){
   return(
 
   <SignIn/>
 
-  )
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StatusBar} from 'react-native';
+
+
+
+
+import { styles } from './style'
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IlustrationImg from '../../assets/illustration.png';
 
 export function SignIn(){
+
   return(
 
-  <View>
-    <Text>Hello World,NLW together!</Text>
+  <View style={styles.container}>
+    <StatusBar 
+      barStyle="light-content" //Deixando a barra Branca, porem ela fica com background
+      backgroundColor='transparent' // Retirando BG mas o conteudo fica abaixo
+      translucent // COlando o conteudo la no topo novamente
+    />
+    <Image source={IlustrationImg} style={styles.image} resizeMode='stretch'/>
+
+    <View style={styles.content}>
+
+      <Text style={styles.title}>
+        Organize {`\n`}
+        suas jogatinas {`\n`}
+        facilmente
+      </Text>
+
+      <Text style={styles.subtitle}>
+        Crie seus grupos para jogar seus games {`\n`}
+        favoritos com seus amigos
+      </Text>
+
+
+    <ButtonIcon/>
+
+    </View>
   </View>
 
   )
