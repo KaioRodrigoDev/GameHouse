3cee471b8718bf2d9a2d57c23a1b1a229fe10878
Rodrigo <kaiorodrigocoder@gmail.com>
2021-11-28 12:40:21 -0300

    finalização da tela de SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, StatusBar} from 'react-native';
+import { View, Text, Image, StatusBar } from 'react-native';
 
 
 
@@ -8,36 +8,40 @@ import { styles } from './style'
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IlustrationImg from '../../assets/illustration.png';
 
-export function SignIn(){
+export function SignIn() {
 
-  return(
+  return (
 
-  <View style={styles.container}>
-    <StatusBar 
-      barStyle="light-content" //Deixando a barra Branca, porem ela fica com background
-      backgroundColor='transparent' // Retirando BG mas o conteudo fica abaixo
-      translucent // COlando o conteudo la no topo novamente
-    />
-    <Image source={IlustrationImg} style={styles.image} resizeMode='stretch'/>
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content" //Deixando a barra Branca, porem ela fica com background
+        backgroundColor='transparent' // Retirando BG mas o conteudo fica abaixo
+        translucent // COlando o conteudo la no topo novamente
+      />
+      <Image source={IlustrationImg} style={styles.image} resizeMode='stretch' />
 
-    <View style={styles.content}>
+      <View style={styles.content}>
 
-      <Text style={styles.title}>
-        Organize {`\n`}
-        suas jogatinas {`\n`}
-        facilmente
-      </Text>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
 
-      <Text style={styles.subtitle}>
-        Crie seus grupos para jogar seus games {`\n`}
-        favoritos com seus amigos
-      </Text>
+        <Text style={styles.subtitle}>
+          Crie seus grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
 
 
-    <ButtonIcon/>
+        <ButtonIcon 
+          title="Entrar com Discord" 
+          activeOpacity={0.7}
+        
+        />
 
+      </View>
     </View>
-  </View>
 
   )
 }
